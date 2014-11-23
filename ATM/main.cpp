cbfa9c1ae53f2d56295bc413b4c80a81a8bdf7a7
xeasony <xeasony@gmail.com>
2014-11-23 16:38:15 +0100

    ATM

--- ATM/main.cpp
+++ ATM/main.cpp
@@ -6,10 +6,9 @@
 //  Copyright (c) 2014 Xeasony. All rights reserved.
 //
 
-#include <iostream>
+#include "ATM.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    ATM atm;
+    atm.run();
 }
